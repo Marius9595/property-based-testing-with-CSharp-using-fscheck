ef6fc135ad66ace194e40d9b78e63ccea9e9fc38
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-08-09 23:08:10 +0100

    ✅ Property FindsBiggestNumber (with model technique)

--- Kata/biggest_number/BiggestNumber.cs
+++ Kata/biggest_number/BiggestNumber.cs
@@ -2,5 +2,16 @@ namespace Kata.biggest_number;
 
 public class BiggestNumber
 {
-    
+    public static int FindBiggestNumber(List<int> numbers)
+    {
+        var biggestNumber = numbers[0];
+        foreach (var number in numbers)
+        {
+            if (biggestNumber < number)
+            {
+                biggestNumber = number;
+            }
+        }
+        return biggestNumber;
+    }
 }
