b17690fc42bf1e50ecf729cd91f95fcda17e62da
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-08-09 23:07:46 +0100

    🧪 Property FindsBiggestNumber (with model technique)

--- Kata.Tests/BiggestNumberShould.cs
+++ Kata.Tests/BiggestNumberShould.cs
@@ -1,11 +1,24 @@
 using FluentAssertions;
+using FsCheck;
+using FsCheck.Fluent;
+using FsCheck.Xunit;
+using Kata.biggest_number;
 
 namespace Kata.Tests;
 
 public class BiggestNumberShould
 {
-    [Fact]
-    public void DoNothing()
+    
+    [Property]
+    public Property FindsBiggestNumber()
     {
+        var generatorOfListOfNumbers = (
+            Gen.Choose(int.MinValue, int.MaxValue).NonEmptyListOf().ToArbitrary()
+        );
+        return Prop.ForAll(generatorOfListOfNumbers, numbers =>
+        {
+            var biggestNumber = BiggestNumber.FindBiggestNumber(numbers);
+            biggestNumber.Should().Be(numbers.Max());
+        });
     }
 }
