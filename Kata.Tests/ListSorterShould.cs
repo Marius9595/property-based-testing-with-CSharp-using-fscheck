030bec25717a4b3f0b9d74ece778a47f5a4c0fe4
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-08-10 00:00:00 +0100

    🧪 Property ASortedListHasOrderedPairs (with invariant technique)

---
+++ Kata.Tests/ListSorterShould.cs
@@ -0,0 +1,40 @@
+using Kata.biggest_number;
+
+namespace Kata.Tests;
+using FluentAssertions;
+using FsCheck;
+using FsCheck.Fluent;
+using FsCheck.Xunit;
+
+public class ListSorterShould
+{
+    [Property]  
+    public Property ASortedListHasOrderedPairs()  
+    {  
+        var generatorOfListOfNumbers = (  
+            Gen.Choose(int.MinValue, int.MaxValue).NonEmptyListOf().ToArbitrary()  
+        );
+        return Prop.ForAll(generatorOfListOfNumbers,numbers =>  
+            {
+                var sortedNumbers = ListSorter.Sort(numbers);
+                IsOrdered(sortedNumbers).Should().BeTrue();
+            }
+        );  
+    }
+    
+    static bool IsOrdered(List<int> numbers)
+    {
+        if (numbers.Count >= 2)
+        {
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i - 1] > numbers[i])
+                {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
---
+++ Kata/ListSorter.cs
@@ -0,0 +1,9 @@
+namespace Kata.biggest_number;
+
+public class ListSorter
+{
+    public static List<int> Sort(List<int> numbers)
+    {
+        return numbers;
+    }
+}
